ca3213610c6d82a26aa08ebf6f55a7d2b3a03719
Nils-Emil Lille <nils-emil.lille@nortal.com>
2019-02-03 16:08:38 +0200

    search start

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,22 +1,32 @@
 import {Component, OnInit} from '@angular/core';
 import {UserHttpService} from '../../services/user.http-service';
+import {User} from '../../models/user.model';
+import {UserSearchModel} from '../../models/user-search.model';
 
 @Component({
-    selector: 'app-users',
-    templateUrl: './users.component.html',
-    styleUrls: ['./users.component.css']
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
 })
 
 export class UsersComponent implements OnInit {
 
-    public users: any[];
+  public users: any[];
+  public searchParams: UserSearchModel = {} as UserSearchModel;
 
-    constructor(private userService: UserHttpService) {
-        this.userService.getAllUsers().subscribe(
-            (response) => this.users = response,
-            () => console.log('i dont know how to handle this error :) We will have some monkeys fix this error soon.'));
+  constructor(private userService: UserHttpService) {
+    this.userService.getAllUsers().subscribe(
+      (response) => this.users = response,
+      () => console.log('i dont know how to handle this error :) We will have some monkeys fix this error soon.'));
+  }
+
+  ngOnInit() {
+  }
+
+  showUser(user: User) {
+    if (user.name.startsWith(this.searchParams.name)) {
+      return true;
     }
-
-    ngOnInit() { }
+  }
 
 }
