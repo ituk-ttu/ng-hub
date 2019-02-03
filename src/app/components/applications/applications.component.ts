9e410446e6888535b550182ce0edb0c72ce5ea10
Nils-Emil Lille <nils-emil.lille@nortal.com>
2019-02-03 03:42:28 +0200

    files i forgot to add because it is so late

---
+++ src/app/components/applications/applications.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {ApplicationHttpService} from '../../services/application.http-service';
+import {ApplicationModel} from '../../models/application.model';
+
+@Component({
+  selector: 'app-applications',
+  templateUrl: './applications.component.html',
+  styleUrls: ['./applications.component.css']
+})
+export class ApplicationsComponent implements OnInit {
+
+  public applications: ApplicationModel[];
+
+  constructor(private applicationHttpService: ApplicationHttpService) {
+  }
+
+  ngOnInit() {
+    this.applicationHttpService.getAllApplications().subscribe((result) => this.applications = result);
+  }
+
+}
---
+++ src/app/components/door-permissions/door-permissions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-door-permissions',
+  templateUrl: './door-permissions.component.html',
+  styleUrls: ['./door-permissions.component.css']
+})
+export class DoorPermissionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
