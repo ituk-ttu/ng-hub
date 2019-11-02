91ff17ac0a1585bcd60a13d4b71559259eb25361
Nils-Emil <nilsemil.lille@gmail.com>
2019-11-02 19:44:45 +0200

    fix not showing name

--- src/app/core/interceptor/httpconfig.interceptor.ts
+++ src/app/core/interceptor/httpconfig.interceptor.ts
@@ -10,8 +10,8 @@ export class NoopInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token: string = localStorage.getItem('token');
-    if (token) {
-      request = request.clone({ headers: request.headers.set('Authorization', token) });
+    if (token && !request.url.includes('/login')) {
+      request = request.clone({ headers: request.headers.set('Authorization', 'Bearer ' +  token) });
     }
 
     if (!request.headers.has('Content-Type')) {
