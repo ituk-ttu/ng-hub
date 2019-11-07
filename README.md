50f6189c9ff94ec09f559accf73f48a1db3fd3ae
Nils-Emil <nilsemil.lille@gmail.com>
2019-11-07 22:04:27 +0200

    add some filtering functionality to DoorBulkAddComponent

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# AngularHub
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.0.
+```
+    _   __                    __              __  
+   / | / /   ____ _          / /_   __  __   / /_ 
+  /  |/ /   / __ `/ ______  / __ \ / / / /  / __ \
+ / /|  /   / /_/ / /_____/ / / / // /_/ /  / /_/ /
+/_/ |_/    \__, /         /_/ /_/ \__,_/  /_.___/ 
+          /____/                                                                       
+```
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli)
 
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Run `ng serve` or `npm start` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
 ## Code scaffolding
 
@@ -14,13 +20,6 @@ Run `ng generate component component-name` to generate a new component. You can 
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
 
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
 
 ## Further help
 
--- src/app/modules/pages/door-bulk-add/door-bulk-add.component.ts
+++ src/app/modules/pages/door-bulk-add/door-bulk-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserHttpService } from '../../../core/http-services/user.http-service';
+import { User } from '../../../shared/models/user.model';
 
 @Component({
   selector: 'app-door-bulk-add',
@@ -6,10 +8,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./door-bulk-add.component.sass']
 })
 export class DoorBulkAddComponent implements OnInit {
+  private users: User[];
+  public usersSearchResult: User[];
+  public selectedUsers: User[] = [];
+  private rooms: string[];
+  public roomsSearchResult: string[];
+  public selectedRooms: string[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private userHttpService: UserHttpService) {
   }
 
+  ngOnInit() {
+    this.userHttpService.getAllUsers().subscribe(e => {
+      this.users = e;
+      this.usersSearchResult = e;
+    });
+    this.rooms = ['ICT-315', 'ICT-316', 'ICT-362', 'ICT-515'];
+    this.roomsSearchResult = [...this.rooms];
+  }
+
+
+  searchRoomsvalue(value: any) {
+    this.roomsSearchResult = [...this.rooms].filter(e => {
+      return (e.includes(value)) && !this.selectedRooms.includes(e);
+    });
+  }
+
+
+  searchUsers(value: any) {
+    this.usersSearchResult = [...this.users].filter(e => {
+      return (e.firstName.includes(value) || e.lastName.includes(value)) && !this.selectedUsers.includes(e);
+    });
+  }
+
+  removeUserFromSelection(user: User) {
+    this.usersSearchResult.push(user);
+    this.selectedUsers = this.selectedUsers.filter(e =>  e !== user);
+  }
+
+  addUserToSelection(user: User) {
+    this.usersSearchResult = this.usersSearchResult.filter(e =>  e !== user);
+    this.selectedUsers.push(user);
+  }
+
+  removeRoomFromSelection(room: string) {
+    this.roomsSearchResult.push(room);
+    this.selectedRooms = this.selectedRooms.filter(e =>  e !== room);
+  }
+
+  addRoomToSelection(room: string) {
+    this.roomsSearchResult = this.roomsSearchResult.filter(e =>  e !== room);
+    this.selectedRooms.push(room);
+  }
+
+  addRoomAccess() {
+    alert('Not implemented!');
+  }
+
+  removeRoomAccess() {
+    alert('Not implemented!');
+  }
 }
