b0fc9a7357307349d6e92c5057b993cc8faeaff7
Raimond Lume <lume.raimond@gmail.com>
2020-03-08 21:39:55 +0200

    Remove project summary fields (will go on a separate form)

--- src/app/modules/pages/projects/projects.component.ts
+++ src/app/modules/pages/projects/projects.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ProjectHttpService} from '../../../core/http-services/project.http-service';
 import * as moment from 'moment';
 import {Project} from '../../../shared/models/project.model';
-import {FormControl, FormGroup, FormArray, FormBuilder, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AuthContext} from '../../../core/services/authContext';
 
 
@@ -16,6 +16,7 @@ export class ProjectsComponent implements OnInit {
   editProjectForm: FormGroup;
   private isEditFormViewActive = false;
   private selectedProject: string;
+  private isMultiDayEvent = false;
 
   constructor(public projectHttpService: ProjectHttpService, private fb: FormBuilder, public auth: AuthContext) {
   }
@@ -40,51 +41,11 @@ export class ProjectsComponent implements OnInit {
   createFormGroup(project?: Project) {
     return this.fb.group({
       title: [project ? project.title : null, Validators.required],
-      multiDayEvent: [''],
       projectLead: [project ? project.projectLead : null, Validators.required],
       dateStart: [project ? project.dateStart : null, Validators.required],
       dateEnd: [project ? project.dateEnd : null],
-      description: [project ? project.description : null, Validators.required],
-      positiveSummary: [project ? project.summary.positiveSummary : null],
-      negativeSummary: [project ? project.summary.negativeSummary : null],
-      members: this.fb.array([
-          this.fb.control(project ? project.members : null)
-      ]),
-      budgetRows: this.fb.array([
-        this.createBudgetRow()
-      ]),
+      description: [project ? project.description : null, Validators.required]
     });
-  }
-
-  get members() {
-    return this.editProjectForm.get('members') as FormArray;
-  }
-
-  createMember(): FormGroup {
-    return this.fb.group({
-      user: null,
-      name: ''
-    });
-  }
-
-  addMember(): void {
-    this.members.push(this.createMember());
-  }
-
-  get budgetRows() {
-    return this.editProjectForm.get('budgetRows') as FormArray;
-  }
-
-  createBudgetRow(): FormGroup {
-    return this.fb.group({
-      description: '',
-      itukCost: '',
-      facultyCost: ''
-    });
-  }
-
-  addBudgetRow(): void {
-    this.budgetRows.push(this.createBudgetRow());
   }
 
   private resetFrom() {
@@ -120,8 +81,4 @@ export class ProjectsComponent implements OnInit {
       moment(project.dateStart).isBefore(moment().subtract(1, 'month'))
     );
   }
-
-
-
-
 }
