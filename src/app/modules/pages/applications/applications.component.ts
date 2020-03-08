81e2c7cbc5eeb9788fa68520ca9d2d3efa4dbdf2
jparve <jparve@ttu.ee>
2020-03-08 17:08:31 +0200

    Filter applications by multiple filters

--- src/app/modules/pages/applications/applications.component.ts
+++ src/app/modules/pages/applications/applications.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApplicationModel } from '../../../shared/models/application.model';
 import { ApplicationHttpService } from '../../../core/http-services/application.http-service';
 import { Router } from '@angular/router';
 
@@ -6,11 +7,53 @@ import { Router } from '@angular/router';
   selector: 'app-applications',
   templateUrl: './applications.component.html'
 })
-export class ApplicationsComponent {
+export class ApplicationsComponent implements OnInit {
 
-  applications = this.applicationHttpService.getAllApplications();
+  applications: ApplicationModel[];
+  applicationsToDisplay: ApplicationModel[];
+
+  filters = {
+    name: '',
+    personalCode: '',
+    email: '',
+    studentCode: '',
+    mentor: '',
+  };
 
   constructor(public applicationHttpService: ApplicationHttpService, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.applicationHttpService.getAllApplications().subscribe(
+        (response) => {
+          this.applications = response;
+          this.applicationsToDisplay = response;
+        },
+        () => console.log('i dont know how to handle this error :) We will have some monkeys fix this error soon.'));
+  }
+
+  filterApplications(filter: string, filterString: string) {
+    this.filters[filter] = filterString;
+    const filterKeys = Object.keys(this.filters);
+
+    this.applicationsToDisplay = this.applications.filter(eachObj => {
+      return filterKeys.every(eachKey => {
+
+        if (!this.filters[eachKey]) {
+          return true;
+        }
+
+        if (eachKey === 'name') {
+          return (eachObj.firstName + ' ' + eachObj.lastName).toLowerCase().includes(this.filters[eachKey]);
+        }
+
+        if (eachKey === 'mentor' && eachObj.mentor) {
+          return (eachObj.mentor.firstName + ' ' + eachObj.mentor.lastName).toLowerCase().includes(this.filters[eachKey]);
+        }
+
+        return eachObj[eachKey] ? eachObj[eachKey].toLowerCase().includes(this.filters[eachKey]) : false;
+      });
+    });
   }
 
   navigateToApplication(application: any) {
