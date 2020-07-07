d1b8a9161c3f27106de817ce403244adaf5a880c
Kaspar Kivistik <kkivis@ttu.ee>
2020-07-07 11:11:06 +0300

    Fixed applications search (#15)

--- src/app/modules/pages/applications/applications.component.ts
+++ src/app/modules/pages/applications/applications.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ApplicationModel } from '../../../shared/models/application.model';
 import { ApplicationHttpService } from '../../../core/http-services/application.http-service';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-applications',
@@ -12,51 +14,61 @@ export class ApplicationsComponent implements OnInit {
   applications: ApplicationModel[];
   applicationsToDisplay: ApplicationModel[];
 
-  filters = {
-    name: '',
-    personalCode: '',
-    email: '',
-    studentCode: '',
-    mentor: '',
-  };
+  searchForm: FormGroup;
 
   constructor(public applicationHttpService: ApplicationHttpService, private router: Router) {
   }
 
   ngOnInit() {
-    this.applicationHttpService.getAllApplications().subscribe(
-        (response) => {
+    this.buildForm();
+    this.applicationHttpService.getAllApplications().subscribe(response => {
           this.applications = response;
           this.applicationsToDisplay = response;
         },
         () => console.log('i dont know how to handle this error :) We will have some monkeys fix this error soon.'));
+
+    this.searchForm.valueChanges
+        .pipe(debounceTime(20), distinctUntilChanged())
+        .subscribe(() => this.filterApplications());
   }
 
-  filterApplications(filter: string, filterString: string) {
-    this.filters[filter] = filterString;
-    const filterKeys = Object.keys(this.filters);
+  filterApplications() {
+    const searchForm = this.searchForm.value;
+    if (!Object.values(searchForm).some(x => x !== null && x !== '')) {
+      this.applicationsToDisplay = this.applications;
+      return;
+    }
+    this.applicationsToDisplay = this.applications.filter(application => {
 
-    this.applicationsToDisplay = this.applications.filter(eachObj => {
-      return filterKeys.every(eachKey => {
-
-        if (!this.filters[eachKey]) {
-          return true;
-        }
-
-        if (eachKey === 'name') {
-          return (eachObj.firstName + ' ' + eachObj.lastName).toLowerCase().includes(this.filters[eachKey]);
-        }
-
-        if (eachKey === 'mentor' && eachObj.mentor) {
-          return (eachObj.mentor.firstName + ' ' + eachObj.mentor.lastName).toLowerCase().includes(this.filters[eachKey]);
-        }
-
-        return eachObj[eachKey] ? eachObj[eachKey].toLowerCase().includes(this.filters[eachKey]) : false;
-      });
+      if (searchForm.name) {
+        return (application.firstName + ' ' + application.lastName).toLowerCase().includes(searchForm.name.toLowerCase());
+      }
+      if (application.mentor && searchForm.mentor) {
+        return (application.mentor.firstName + ' ' + application.mentor.lastName).toLowerCase().includes(searchForm.mentor.toLowerCase());
+      }
+      if (searchForm.personalCode) {
+        return application.personalCode.toLowerCase().includes(searchForm.personalCode.toLowerCase());
+      }
+      if (searchForm.email) {
+        return application.email.toLowerCase().includes(searchForm.email.toLowerCase());
+      }
+      if (searchForm.studentCode) {
+        return application.studentCode.toLowerCase().includes(searchForm.studentCode.toLowerCase());
+      }
     });
   }
 
   navigateToApplication(application: any) {
     this.router.navigate([`hub/applications/${application.id}`]);
+  }
+
+  private buildForm() {
+    this.searchForm = new FormGroup({
+      name: new FormControl(),
+      personalCode: new FormControl(),
+      email: new FormControl(),
+      studentCode: new FormControl(),
+      mentor: new FormControl(),
+    });
   }
 }
