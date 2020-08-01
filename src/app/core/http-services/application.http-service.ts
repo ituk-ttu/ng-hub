69ef560d532390e8592a932b490535c485930df5
Kaspar Kivistik <kaspar.kivistik@monese.com>
2020-08-01 13:23:28 +0300

    accept and refuse applications

--- src/app/core/http-services/application.http-service.ts
+++ src/app/core/http-services/application.http-service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { ApplicationModel } from '../../shared/models/application.model';
 import { Observable } from 'rxjs';
+import { ApplicationStatus } from '../../shared/models/application-status.enum';
 
 @Injectable()
 export class ApplicationHttpService {
@@ -23,6 +24,13 @@ export class ApplicationHttpService {
   public deleteApplication(id: number) {
     const url = `${environment.API_URL}/application/${id}`;
     return this.http.delete(url);
+  }
+
+  public changeApplicationStatus(id: number, status: ApplicationStatus) {
+    const url = `${environment.API_URL}/application/${id}/status`;
+    return this.http.put(url, {
+      status: status.toString()
+    });
   }
 
 }
