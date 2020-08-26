aa7b4cfe5f8274aba86b96c80bf091910b389c20
nilsemil <nilsemil.lille@gmail.com>
2020-08-26 19:48:43 +0300

    remove import

--- src/app/core/http-services/mentor-profiles.http-service.ts
+++ src/app/core/http-services/mentor-profiles.http-service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpHeaders } from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {MentorProfileModel} from '../../shared/models/mentor-profile.model';
 import {environment} from '../../../environments/environment';
